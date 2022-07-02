4cb36a6b20d42c915b09874f91790f1de658d166
aXingDocker <axing_1207@163.com>
2022-07-03 06:42:05 +0800

    成功部署到宿主机

--- bin/setup_host.sh
+++ bin/setup_host.sh
@@ -10,7 +10,9 @@ if [ "$(docker ps -aq -f name=^mangosteen-prod-1$)" ]; then
   docker rm -f $container_name
 fi
 echo 'docker run ...'
-docker run -d -p 3000:3000 --network=network1 -e DB_PASSWORD=$DB_PASSWORD --name=$container_name mangosteen:$version
+docker run -e DB_HOST=$DB_HOST -e RAILS_MASTER_KEY=$RAILS_MASTER_KEY -d -p 3000:3000 --network=network1 -e DB_PASSWORD=$DB_PASSWORD --name=$container_name mangosteen:$version
+echo 'DONE!' 
+
 # echo 'docker exec ...'
 # docker exec -it $container_name bin/rails db:create db:migrate
 echo 'DONE!' 
